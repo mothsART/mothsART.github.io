9fe96867fb7a361fa3e05d87367de345729295c0
Ferry Jérémie <ferryjeremie@free.fr>
2017-06-20 23:44:07 +0200

    Mise à jour de Edit interactive SVG à la version 0.3

--- labo/frontend/edit_interactive_svg/TODO.md
+++ labo/frontend/edit_interactive_svg/TODO.md
@@ -2,7 +2,7 @@
 
 ## Version 1.0
 
-* i18n
+* i18n (with vue.js)
 
 ## Version 2.0
 
@@ -10,3 +10,16 @@
 * Store a list of recent document
 * Import verification on product and static (svg) file
 * full vanilla js (delete jquery)
+
+* Un vrai dialogue d'aide
+* une zone de saisie pour le déplacement d'un indice
+
+# Bugs
+
+* zoom : afficher un sablier au dessus de la zone svg car ce n'est pas instantanné.
+* drag and drop de l'ordre des indices
+* l'affichage de la sidebar
+* le menu à l'affichage
+* le clic sur un indice
+* les soucis de zoom
+* le drag and drop pendant un zoom
--- labo/frontend/edit_interactive_svg/static/export.js
+++ labo/frontend/edit_interactive_svg/static/export.js
@@ -1,47 +1,51 @@
 var get_blob;
 
 (function(view) {
-    "use strict";
-    get_blob = function() {
-        return view.Blob;
-    }
-    document.getElementById("save-form").addEventListener("submit", export_html);
-    dragula($("#list-of-legend"));
-
-    $('#indice-description').trumbowyg();
+  "use strict";
+  get_blob = function() {
+    return view.Blob;
+  }
+  document.getElementById("save-form").addEventListener("submit", export_html);
+  dragula($("#list-of-legend"));
+  $('#indice-description').trumbowyg();
 }(self));
 
-function export_html(event) {
-    "using strict";
-    event.preventDefault();
-    var BB = get_blob();
-    $.when(
-        $.ajax("static/export_style.css"),
-        $.ajax("./static/jquery.min.js"),
-        $.ajax("static/export_js.js")
-    )
-    .done(function(style, jquery_file, js_file){
-        saveAs(
-          new BB(
-              [
-              "<!DOCTYPE html>\n<html>\n<head>\n"
-              + '<style type="text/css">\n'
-              + style[0]
-              + "\n</style>\n"
-              + "</head>\n<body>\n"
-              + '<div id="svg">'
-              + $("#svg").clone().html()
-              + "</div>"
-              + '<script type="text/javascript">\n'
-              + jquery_file[0]
-              + '\n</script>\n'
-              + '<script type="text/javascript">\n'
-              + js_file[0]
-              + "</script>\n"
-              + "</body>\n</html>\n"]
-            , {type: "application/xhtml+xml;charset=UTF-8"}
-        )
-        , "interactive_illustration.html"
-        );
-    });
+function export_html() {
+  "use strict";
+  var BB = get_blob();
+  var svg = $("#svg").clone();
+  svg.find("svg").removeAttr("onmousedown").removeAttr("onmousemove").removeAttr("onmouseup");
+  svg.find("#delete-svg").remove();
+  svg.find("#real-legend").removeClass("hidden");
+  var str_svg = svg.html();
+  $.when(
+    $.ajax("static/export_style.css"),
+    $.ajax("./static/jquery.min.js"),
+    $.ajax("static/export_js.js")
+  )
+  .done(function(style, jquery_file, js_file) {
+    saveAs(
+      new BB(
+        [
+          "<!DOCTYPE html>\n<html>\n<head>\n"
+          + '<style type="text/css">\n'
+          + style[0]
+          + "\n</style>\n"
+          + "</head>\n<body>\n"
+          + '<div id="svg" class="show">\n'
+          + str_svg
+          + "</div>\n"
+          + '<script type="text/javascript">\n'
+          + jquery_file[0]
+          + '\n</script>\n'
+          + '<script type="text/javascript">\n'
+          + js_file[0]
+          + "</script>\n"
+          + "</body>\n</html>\n"
+        ]
+        , {type: "application/xhtml+xml;charset=UTF-8"}
+     )
+     , "interactive_illustration.html"
+    );
+  });
 }
