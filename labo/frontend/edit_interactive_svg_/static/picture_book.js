8bee602cb2105b5ff8f249860f6c4d0d018336bf
Ferry Jérémie <ferryjeremie@free.fr>
2017-12-04 21:23:51 +0100

    Ajout d'une illustration interactive sur les fruits

--- labo/frontend/edit_interactive_svg_/static/picture_book.js
+++
@@ -1,8 +0,0 @@
-function select_theme(element) {
-    "use strict";
-    var themes = document.getElementsByTagName("article");
-    for (var i = 0, len = themes.length; i < len; i++) {
-        themes[i].style.transform = "";
-    }
-    element.parentNode.parentNode.style.transform = "scale(2) translate(-25%, -50%)";
-}
